a2d87a515a9e5ba62eb722533dfb78749b98033b
jennylsmith <jennyl.smith12@gmail.com>
2021-06-03 20:09:02 -0700

    added pagedown

--- project_setup.r
+++ project_setup.r
@@ -1,6 +1,7 @@
 library(usethis)
 library(here)
-
+#remotes::install_github('rstudio/pagedown')
+library(pagedown)
 
 report_path <- here("omics_clinical_reporting/")
 create_project(report_path)
@@ -9,3 +10,6 @@ create_project(report_path)
 use_rmarkdown_template(template_name = "create_omics_report",
                        template_description = "A quick overview of producing a clinical report from multi-omics sequencing data.")
 
+
+#Will use this for the template
+# pagedown::poster_relaxed()
